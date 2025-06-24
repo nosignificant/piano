51d0dc4e43659bac6ceed427d4cca74451f1d30b
nosignificant <nosignificantment@gmail.com>
2025-06-24 22:32:05 +0900

    .

--- src/app/component/Explain.tsx
+++ src/app/component/Explain.tsx
@@ -8,20 +8,26 @@ export default function Explain() {
     <div className="consolas">
       <div className="Bodoni" onClick={openThis}>
         what is this site?
-        {isOpen && (
-          <div className="w-[500px] consolas ">
-            This was inspired by{" "}
-            <a
-              className="border-b border-solid"
-              href="https://100r.co/site/orca.html"
-            >
-              ORCΛ
-            </a>
-            , a two-dimensional esoteric programming language. I wondered what
-            if letter converted into sound and make music. write down letters
-            above.
-          </div>
-        )}
+        <div
+          className={`transition-all duration-500 overflow-hidden ${
+            isOpen ? "max-h-[300px] opacity-100" : "max-h-0 opacity-0"
+          }`}
+        >
+          {isOpen && (
+            <div className="w-[500px] consolas ">
+              This was inspired by{" "}
+              <a
+                className="border-b border-solid"
+                href="https://100r.co/site/orca.html"
+              >
+                ORCΛ
+              </a>
+              , a two-dimensional esoteric programming language. I wondered what
+              if letter converted into sound and make music. write down letters
+              above.
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
